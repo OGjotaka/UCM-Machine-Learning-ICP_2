ecfa6c33fe520d3a1c8c93f76d8ad00b820cd569
OGjotaka <68663897+OGjotaka@users.noreply.github.com>
2024-04-29 17:58:58 -0500

    Delete ICP_2_Q_1.py

--- ICP_2_Q_1.py
+++
@@ -1,9 +0,0 @@
-Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: /Users/jjmholbrook/Desktop/School/Machine Learning/ICP_2/ICP_2_Q_1.py
-*
-*
-*
-*
-*
