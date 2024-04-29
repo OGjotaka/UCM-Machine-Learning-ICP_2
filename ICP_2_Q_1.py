4c19fb6d1c285bbb2f86cc58b266d68ad8af5171
OGjotaka <68663897+OGjotaka@users.noreply.github.com>
2024-04-29 17:59:41 -0500

    Add files via upload

---
+++ ICP_2_Q_1.py
@@ -0,0 +1,15 @@
+#CS 4710
+#JJ Holbrook
+#700514202
+#ICP 2 Question 1
+
+'''
+Use a python code to display the following star pattern using the for loop
+'''
+s = f"*"
+
+for i in range(1, 6):
+    print(s * i)
+for i in range(4, 0, -1):
+    print(s * i)
+
